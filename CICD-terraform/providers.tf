e83213182e9071b40f38aeb5628fa33522b94e70
teju famuyiwa <104201952+tee118@users.noreply.github.com>
2024-03-06 14:01:07 +0000

    test

--- CICD-terraform/providers.tf
+++ CICD-terraform/providers.tf
@@ -1,7 +1,7 @@
 terraform {
      backend "s3" {
-       bucket = "[AWS_S3_BUCKET]"
-       key    = "[AWS_BUCKET_KEY_NAME]"
+       bucket = "AWS_S3_BUCKET"
+       key    = "AWS_BUCKET_KEY_NAME"
        region = "eu-west-2"
   }
 }
