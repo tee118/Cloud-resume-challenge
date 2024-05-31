e472340f5c2c06b2ab4e73e00d507a37661844c6
tee118 <tejufamuyiwa@gmail.com>
2024-05-31 16:22:39 +0100

    terraform update

--- CICD-terraform/providers.tf
+++ CICD-terraform/providers.tf
@@ -1,7 +1,13 @@
-terraform {
-  backend "s3" {
-    bucket = "tejusite"
-    key    = "infra.tfstate"
-    region = "eu-west-2"
-  }
-}
+- id: backend-config
+  uses: amannm/terraform-s3-backend-action@v1
+  with:
+    account-id: "171139160358"
+    region: "eu-west-2"
+    role: "github-cloud-resume-counter-config-tfstate"
+    bucket: "tejusite"
+    key: "infra.tfstate"
+- name: Generate backend config
+  run: |
+    echo "terraform { backend \"s3\" { bucket = \"${{ steps.backend-config.outputs.bucket }}\" key = \"${{ steps.backend-config.outputs.key }}\" region = \"${{ steps.backend-config.outputs.region }}\" } }" > backend.tf
+- name: Initialize Terraform
+  run: terraform init
